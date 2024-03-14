0f062f24bd745a00e8e42185a6c501f3f0b649e7
Viaasyafrina16 <syafrinavia@gmail.com>
2024-03-14 15:27:37 +0700

    menambahkan step 4b

--- praktikum 2 algoritma/praktikum 2 algoritma.cpp
+++ praktikum 2 algoritma/praktikum 2 algoritma.cpp
@@ -39,7 +39,7 @@ void insertionSort() {
         while (j >= 0 && arr[j] > temp)// step 4
         {
             arr[j + 1] = arr[j]; //step 4a
-            j--; //step 4b
+			j--; //step 4b
         }
     
         arr[j + 1] = temp;//step 5
